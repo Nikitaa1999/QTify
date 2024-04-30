5f300accc95220bde4f48d088161696225911f13
unknown <nikita.phulkar@gmail.com>
2024-04-30 12:26:03 +0530

    Navbar

---
+++ src/components/Searchbox/Searchbox.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import styles from "./Searchbox.module.css";
+import {ReactComponent as SearchIcon} from "../../assets/Searchicon.svg";
+export default function Searchbox({ placeholder }){
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+    return(
+        <>
+            <form onSubmit={onSubmit} className={styles.searchContainer}>
+            <input type="text" className={styles.searchBar} id="searchBar" placeholder={placeholder} />
+            <button className={styles.searchButton} type="submit" >
+                <SearchIcon />
+            </button>
+          
+            </form>
+        </>
+    )
+}
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import styles from "./Button.module.css"
+
+function Button({children}){
+    return <button className={styles.button}>{children}</button>
+}
